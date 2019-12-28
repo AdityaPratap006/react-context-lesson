4cdb2e310322143c85c6ae5e70199c521b4168ae
Aditya Pratap <adityapratap7344@gmail.com>
2019-12-28 17:30:57 +0530

    changed firebase config for my project

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyCDig1POG_rEeqaK1h3jrLs2cg1hHO7oGM",
+  authDomain: "crown-db-11f3a.firebaseapp.com",
+  databaseURL: "https://crown-db-11f3a.firebaseio.com",
+  projectId: "crown-db-11f3a",
+  storageBucket: "crown-db-11f3a.appspot.com",
+  messagingSenderId: "105276876812",
+  appId: "1:105276876812:web:1a9826a4b4414cc2"
 };
 
 firebase.initializeApp(config);
